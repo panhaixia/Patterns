26522a05b65c45370f194032878b62b256f1a541
panhaixia <panhaixia@yaochufa.com>
2016-08-30 14:17:01 +0800

    编程作业

--- Sorting algorithm/BubbleSort.cpp
+++ Sorting algorithm/BubbleSort.cpp
@@ -1,21 +1,37 @@
-public static void BubbleSort(int[] R)
+#include<iostream>
+using namespace std;
+void bubbleSort(int a[],int size);
+int main()
 {
-    for (int i = 0; i < R.Length - 1; i++)
-    {
-        bool noswap = true;
-        for (int j = 0; j < R.Length - 1-i; j++)
-        {
-            if (R[j] > R[j + 1])
-            {
-                int temp = R[j];
-                R[j] = R[j + 1];
-                R[j + 1] = temp;
-                noswap = false;
-            }
-        }
-        if (noswap)
-        {
-            break;
-        }
-    }
+   int array[]={34,65,12,43,67,5,78,10,3,70},k;
+   int len=sizeof(array)/sizeof(int);
+   cout<<"The orginal array are:"<<endl;
+   for(k=0;k<len;k++)
+      cout<<array[k]<<",";
+   cout<<endl;
+   bubbleSort(array,len);
+   cout<<"The sorted array are:<<endl";
+   for(k=0;k<len;k++)
+      cout<<array[k]<<",";
+   cout<<endl;
+   system("pause");
+   return 0;
+}
+void bubbleSort(int a[],int size)
+{
+     int temp;
+     for(int pass=1;pass<size;pass++)
+     {
+        for(int k=0;k<size-pass;k++)
+           if(a[k]>a[k+1])
+           {
+                temp=a[k];
+                a[k]=a[k+1];
+                a[k+1]=temp;
+           }
+        cout<<"µÚ"<<pass<<"ÂÖÅÅÐòºó£º";
+        for(int i=0;i<size;i++)
+          cout<<a[i]<<",";
+        cout<<endl;
+     }
 }
